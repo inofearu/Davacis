2822ad972d86b6442246d3de35fb7faa7eb517bc
Inofearu <inofearu@gmail.com>
2023-04-24 13:52:46 +0100

    Jumping implemented
    Problems:
    Jumping Floaty
    cc.isGrounded
    Fixed:
    No Y acceleration fixed by using Vector 3s.
    Implemented:
    Jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    [SerializeField] GameObject playerCam;
+    [SerializeField] GameObject playerEyes;
+    [SerializeField] float jumpHeight = 1f;
     [SerializeField] float walkSpeed = 5f;
     [SerializeField] float sprintSpeed = 7.5f;
     [SerializeField] float lookSpeed = 1f;
     [SerializeField] float maxYLookAngle = 30f;
     [SerializeField] float minYLookAngle = -30f;
+    Vector3 verticalMove = Vector3.zero;
     private float yAxis = 0f;
     private float xAxis = 0f;
     Vector3 playerVelocity = Vector3.zero;
-    //[SerializeField] float jumpHeight = 1f;
     private CharacterController cc;
     private float playerSpeed;
     // Start is called before the first frame update
@@ -36,36 +37,39 @@ public class PlayerMovement : MonoBehaviour
         yAxis += (Input.GetAxis("Mouse Y") * lookSpeed);
         xAxis += (Input.GetAxis("Mouse X") * lookSpeed);
         yAxis = Mathf.Clamp(yAxis, maxYLookAngle * -1, minYLookAngle * -1); // values flipped so that you dont need to provide a - value for max
-        gameObject.transform.eulerAngles = new Vector3(yAxis, xAxis, 0);
+
+        gameObject.transform.eulerAngles = new Vector3(0, xAxis, 0);
+        playerEyes.transform.eulerAngles = new Vector3(yAxis,xAxis,0);
     }
 
     private void Move()
     {
-        bool isGrounded;
-        float moveSpeed;
-
-        if (Input.GetKey(KeyCode.LeftShift))
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+        float forwardMove = Input.GetAxisRaw("Forward");
+        verticalMove += new Vector3 (0,playerVelocity.y + -9.81f * Time.deltaTime,0);
+        Debug.Log(verticalMove);
+        if(cc.isGrounded)
         {
-            moveSpeed = sprintSpeed;
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                verticalMove += new Vector3(0,jumpHeight,0);
+            }
+            else
+            {
+                verticalMove = Vector3.zero;
+            }
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            playerVelocity = new Vector3 (horizontalMove,0,forwardMove).normalized * sprintSpeed;
         }
         else
         {
-            moveSpeed = walkSpeed;
-        }
-        isGrounded = cc.isGrounded;
-
-        float horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
-        float forwardMove = Input.GetAxisRaw("Forward") * moveSpeed;
-        float verticalMove = playerVelocity.y + -9.81f * Time.deltaTime;
-
-        if (isGrounded && cc.velocity.y < 0.1)
-        {
-            verticalMove = 0f;
+            playerVelocity = new Vector3 (horizontalMove,0,forwardMove).normalized * walkSpeed;
         }
 
-        playerVelocity = new Vector3 (horizontalMove,0,forwardMove);
         cc.Move(transform.TransformDirection(playerVelocity) * Time.deltaTime);
-        cc.Move(new Vector3(0,verticalMove,0));
+        cc.Move(verticalMove * Time.deltaTime);
     }
 }
-
