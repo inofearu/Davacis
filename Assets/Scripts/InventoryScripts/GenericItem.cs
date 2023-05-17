a2ef2c75614cca65d9782b8d6e450b9a77c3350d
Inofearu <inofearu@gmail.com>
2023-05-17 14:39:38 +0100

    Change to single file item track

--- Assets/Scripts/InventoryScripts/GenericItem.cs
+++ Assets/Scripts/InventoryScripts/GenericItem.cs
@@ -2,14 +2,4 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "GenericItem.asset", menuName = "Inventory/Item/Generic")]
-public class GenericItem : ScriptableObject
-{
-    public int itemID;
-    public string itemName;
-    public string itemDescription;
-    public GameObject itemModel;
-    public int value;
-    public GameObject gameObject;
 
-}
---
+++ Assets/Scripts/InventoryScripts/ItemDatabase.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GenericItem.asset", menuName = "Inventory/Item/Generic")]
+public class GenericItem : ScriptableObject
+{
+    public int itemID;
+    public string itemName;
+    public string itemDescription;
+    public GameObject itemModel;
+    public int goldValue;
+    public GameObject gameObject;
+}
+
+[CreateAssetMenu(fileName = "Weapon.asset", menuName = "Inventory/Item/Weapon")]
+public class WeaponItem : GenericItem
+{
+    private GameObject Projectile;
+    public int damage;
+    public int destructionDamage;
+    public float speed;
+    //[SerializeField] float projectileRadius;
+    public float damageRadius;
+    public float destructionRadius;
+    public bool useGravity;
+    public float softLife;
+    public float hardLife;
+    public int manaUse;
+    public float cooldown;
+}
+[System.Serializable]
+public class ItemDatabase : MonoBehaviour
+{
+    public List<GenericItem> ItemList;
+    void Start()
+    {
+    }
+}
