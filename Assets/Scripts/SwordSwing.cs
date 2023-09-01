5a25f6738a94ccac33a6b38c063b65a59c213e74
Inofearu <inofearu@gmail.com>
2023-09-01 18:43:04 +0100

    LineRender instead of Debug.Drawline
    This allows it to render in game view

--- Assets/Scripts/SwordSwing.cs
+++ Assets/Scripts/SwordSwing.cs
@@ -9,22 +9,51 @@ public class SwordSwing : MonoBehaviour
     public float hitCooldown;
     public float hitRange;
     private float timeForNextHit;
-
-    void Update()
+    public bool showSpherecastDebug; // PLACEHOLDER: Implement in-game toggle
+    private LineRenderer lineRenderer;
+   void Update()
     {
         if (Input.GetMouseButtonDown(0) && Time.time > timeForNextHit)
         {
-            Debug.Log("1");
-            if (Physics.SphereCast(Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 10.0f)), hitRadius, transform.forward, out RaycastHit hitData, hitRange))
+            if (showSpherecastDebug)
             {
-                Debug.Log("2");
+                DrawSphereCast(Color.green);
+            }
+
+            if (Physics.SphereCast(Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, Camera.main.nearClipPlane)), hitRadius, transform.forward, out RaycastHit hitData, hitRange))
+            {
+                if (showSpherecastDebug)
+                {
+                    DrawSphereCast(Color.yellow);
+                }
+
                 IHit hitResponder = hitData.collider.gameObject.GetComponent<IHit>();
                 if (hitResponder != null)
                 {
-                    Debug.Log("3");
+                    if (showSpherecastDebug)
+                    {
+                        DrawSphereCast(Color.red);
+                    }
                     hitResponder.OnHit(hitData);
                 }
             }
         }
+    }
+    private void Awake()
+    {
+        lineRenderer = gameObject.AddComponent<LineRenderer>(); // PLACEHOLDER: Implement in-game toggle
+        lineRenderer.positionCount = 2;
+        lineRenderer.material = new Material(Shader.Find("Legacy Shaders/Particles/Alpha Blended Premultiply"));
+    }
+    private void DrawSphereCast(Color color)
+    {
+        Vector3 startPoint = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, Camera.main.nearClipPlane));
+        Vector3 endPoint = startPoint + Camera.main.transform.forward * hitRange;
+        lineRenderer.widthMultiplier = hitRadius;
+        lineRenderer.SetPosition(0, startPoint);
+        lineRenderer.SetPosition(1, endPoint);
+        lineRenderer.startColor = color;
+        lineRenderer.endColor = color;
+        Debug.Log($"{startPoint} - {endPoint}");
     }
 }
