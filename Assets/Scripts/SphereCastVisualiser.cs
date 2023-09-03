1f016cc09f2b3969ee4fbfe1fec134e1744b83fe
Inofearu <inofearu@gmail.com>
2023-09-03 16:40:51 +0100

    Added var to limit number of draws

--- Assets/Scripts/SphereCastVisualiser.cs
+++ Assets/Scripts/SphereCastVisualiser.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class SphereCastVisualiser : MonoBehaviour
 {
+    public int maxCasts = 3; // TODO: implement in-game switch
     public GameObject capsulePrefab;
+    private Queue<GameObject> drawnObjects;
     public void Draw(Color color, float range, float radius, Collider hitObj)
     {
         Vector3 startPoint = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, Camera.main.nearClipPlane));
@@ -15,6 +17,19 @@ public class SphereCastVisualiser : MonoBehaviour
         Renderer capsuleRenderer = capsule.GetComponent<Renderer>();
         capsuleRenderer.material.SetColor("_Color", color);
         capsule.transform.localScale = new Vector3(radius * 2, range / 2, radius * 2);
+
+        drawnObjects.Enqueue(capsule);
+        if (drawnObjects.Count > maxCasts) // remove old casts
+        {
+            Destroy(drawnObjects.Dequeue());
+        }
+
+
         Debug.Log($"|{startPoint} - {endPoint}|, {hitObj}");
+    }
+
+    private void Awake()
+    {
+        drawnObjects = new Queue<GameObject>();
     }
 }
